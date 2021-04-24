020a004b8d7cf4ac6cce774b6c5d76249c9bbc7f
kailikia <edwinkailikia@gmail.com>
2021-04-24 15:15:24 +0300

    new deployment 2

--- src/main/java/com/duka/sales/controllers/SalesController.java
+++ src/main/java/com/duka/sales/controllers/SalesController.java
@@ -1,13 +1,9 @@
 package com.duka.sales.controllers;
 
-import java.io.Serializable;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.HashMap;
+
 import java.util.List;
 
 import org.springframework.amqp.core.AmqpTemplate;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -50,7 +46,7 @@ public class SalesController {
 	 @GetMapping("/sales")
 	 public List<Sale> getAllSales() 
 	 {
-		logger("Browser", "All Sales Records fetched"); 
+		logger("All Sales Records fetched"); 
 	  	return salesrepo.findAll();        
 	 }
 	 
@@ -58,7 +54,7 @@ public class SalesController {
 	 public List<Sale> getAllSalesByInventory(@PathVariable String inv_id ) 
 	 {
 		 int id = new Integer(inv_id);
-		 logger("Browser", "Sales Records fetched for inventory id "+ inv_id);
+		 logger("Sales Records fetched for inventory id "+ inv_id);
 
 	  	 return salesrepo.findByInvId(id);        
 	 }
@@ -72,28 +68,22 @@ public class SalesController {
 			 try {
 				 this.inventory = restTemplate.getForObject(full_url, Inventory.class);
 				 Sale s = salesrepo.save(sale);
-				 logger("Browser", "Successfull Save Sale Record No.: " + s.getId());
+				 logger("Successfull Save Sale Record No.: " + s.getId());
 				 return s;
 			 
 			 }catch (Exception e) {
 				
-				 logger("Browser", "Failed Save Sale Record Inventory No.: " + sale.getInvId());
+				 logger("Failed Save Sale Record Inventory No.: " + sale.getInvId());
 				 throw new ResourceNotFoundException("inventory with id " + sale.getInvId() + " not found");
 			}
 			 		 
 
 	 }
 	 
-	 public String logger(String useragent,String message)
+	 public String logger(String message)
 	 {
-		 StringBuffer mystring = new StringBuffer(); 
-		 mystring.append(new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date()));
-		 mystring.append(",");
-		 mystring.append(message);
-		 mystring.append(",");
-		 mystring.append(useragent);
 		 		 
-		 amqpTemplate.convertAndSend(queue.getName(), new String(mystring));
+		 amqpTemplate.convertAndSend(queue.getName(), new String(message));
 		 return "successfully logged"; 
 	 }
 	 
